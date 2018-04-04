2a4480557a2beded6699471d94c6230b5a073e8c
wikenator <arnold.wikey@gmail.com>
2018-04-04 13:56:52 -0700

    find math question duplicates

--- corpus_stats.py
+++ corpus_stats.py
@@ -6,6 +6,7 @@ import nltk
 from Utils import Utils as Util
 
 vocab = {}
+sents = {}
 counts = []
 alg_count = 0
 arith_count = 0
@@ -28,6 +29,9 @@ for f in files:
 			elif cat == 'arithmetic': arith_count += 1
 			elif cat == 'geometry': geo_count += 1
 
+			if sent not in sents: sents[sent] = 0
+
+			sents[sent] += 1
 			ques_count += 1
 
 			s_tok = nltk.tokenize.sent_tokenize(sent)
@@ -51,3 +55,7 @@ print '\nAvg sents/ques: ' + str(float(sum(counts[:, 0]))/ques_count)
 print 'Avg words/sent: ' + str(float(sum(counts[:, 1]))/float(sum(counts[:, 0])))
 print '\nVocabulary size: ' + str(len(vocab))
 print 'Data size: ' + str(sum(vocab.values()))
+print '\nDuplicates: %d' % (sum([int(v) for v in sents.values() if int(v) > 1]))
+
+for k, v in sents.iteritems():
+	if int(v) > 1: print "%d: %s" % (v, k)
